923643d0ba15a15320e7593093d3cffd7eebd38c
ADavlatov <a@davlatov.space>
2024-09-03 16:53:19 +0300

    Add shapes

---
+++ MindboxTest/Shapes/Circle.cs
@@ -0,0 +1,23 @@
+﻿using MindboxTest.Interfaces;
+
+namespace MindboxTest.Shapes;
+
+public class Circle : IShape
+{
+    private readonly double _radius;
+
+    public Circle(double radius)
+    {
+        if (radius < 0)
+        {
+            throw new ArgumentOutOfRangeException("Radius can't be negative");
+        }
+
+        _radius = radius;
+    }
+
+    public double CalculateArea()
+    {
+        return Math.PI * Math.Pow(_radius, 2);
+    }
+}
---
+++ MindboxTest/Shapes/Triangle.cs
@@ -0,0 +1,43 @@
+﻿using MindboxTest.Interfaces;
+
+namespace MindboxTest.Shapes;
+
+public class Triangle : IShape
+{
+    private readonly double[] _sides;
+
+    public Triangle(double sideA, double sideB, double sideC)
+    {
+        if (sideA < 0 || sideB < 0 || sideC < 0)
+        {
+            throw new ArgumentException("Side lenght can't be negative");
+        }
+
+        _sides = [sideA, sideB, sideC];
+        Array.Sort(_sides);
+
+        if (_sides[0] + _sides[1] <= _sides[2])
+        {
+            throw new ArgumentException("This is not triangle");
+        }
+    }
+
+    public double CalculateArea()
+    {
+        if (IsTriangleRight())
+        {
+            return 0.5 * (_sides[0] * _sides[1]);
+        }
+
+        var halfMeter = (_sides[0] + _sides[1] + _sides[2]) / 2;
+
+        return Math.Sqrt(halfMeter * (halfMeter - _sides[0]) *
+                         (halfMeter - _sides[1]) *
+                         (halfMeter - _sides[2]));
+    }
+
+    public bool IsTriangleRight()
+    {
+        return Math.Abs(Math.Abs(Math.Pow(_sides[0], 2) + Math.Pow(_sides[1], 2)) - Math.Pow(_sides[2], 2)) < 1e-10;
+    }
+}
