aab7086b3073f4ad46dde2df7ebb9fc2509fac9c
ADavlatov <a@davlatov.space>
2024-09-03 16:53:57 +0300

    Add tests

---
+++ MindboxTest.Tests/CircleAreaTest.cs
@@ -0,0 +1,13 @@
+﻿using MindboxTest.Shapes;
+
+namespace MindboxTest.Tests;
+
+public class CircleAreaTest
+{
+    [Fact]
+    public void TestCircleArea()
+    {
+        var circle = new Circle(5);
+        Assert.Equal(Math.PI * 25, circle.CalculateArea(), 1e-10);
+    }
+}
---
+++ MindboxTest.Tests/TriangleAreaTest.cs
@@ -0,0 +1,23 @@
+﻿using MindboxTest.Shapes;
+
+namespace MindboxTest.Tests;
+
+public class TriangleAreaTest
+{
+    [Fact]
+    public void TestCircleArea()
+    {
+        var triangle = new Triangle(3, 4, 5);
+        Assert.Equal(6, triangle.CalculateArea(), 1e-10);
+    }
+    
+    [Fact]
+    public void TestTriangleIsReght()
+    {
+        var triangle = new Triangle(3, 4, 5);
+        Assert.True(triangle.IsTriangleRight());
+        
+        var nonRightTriangle = new Triangle(2, 2, 3);
+        Assert.False(nonRightTriangle.IsTriangleRight());
+    }
+}
